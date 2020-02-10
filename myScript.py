4fdc0fd06498ab8cf6ba964930ca9fb3c5579e6d
Tom Satterthwaite <tom.satters@gmail.com>
2020-02-10 11:31:56 -0800

    changed the size of the identity matrix and second matrix

--- myScript.py
+++ myScript.py
@@ -1,7 +1,6 @@
 import numpy as np
+import pandas as pd
 
-my_matrix = np.eye(4)
-
-my_second_matrix = np.random.rand(4,4)
-
+my_matrix = np.eye(5)
+my_second_matrix = np.random.rand(5,4)
 print(my_second_matrix.dot(my_matrix))
