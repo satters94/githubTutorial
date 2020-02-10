a9a8e2a20c5b0695f447402cf0ecadeb43375cba
Tom Satterthwaite <tom.satters@gmail.com>
2020-02-10 11:21:15 -0800

    created the first version of the function

--- myScript.py
+++ myScript.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+
+def my_func(matrix_1, matrix_2):
+    return matrix_1 * matrix_2
+
 my_matrix = np.eye(4)
+my_second_matrix = np.random.rand(4,5)
 
-my_second_matrix = np.random.rand(4,4)
-
-print(my_second_matrix.dot(my_matrix))
+print(my_func(my_matrix, my_second_matrix)
