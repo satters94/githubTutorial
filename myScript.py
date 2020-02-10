50fae20cf03087bc4378019ed8177cd4e1cbefe2
Tom Satterthwaite <tom.satters@gmail.com>
2020-02-10 11:26:48 -0800

    fixed the funciton using a try/except block for different sized matrices

--- myScript.py
+++ myScript.py
@@ -2,9 +2,12 @@ import numpy as np
 
 
 def my_func(matrix_1, matrix_2):
-    return matrix_1 * matrix_2
+    try:
+        return matrix_1 * matrix_2
+    except ValueError:
+        return None
 
 my_matrix = np.eye(4)
-my_second_matrix = np.random.rand(4,5)
+my_second_matrix = np.random.rand(4,4)
 
-print(my_func(my_matrix, my_second_matrix)
+print(my_func(my_matrix, my_second_matrix))
